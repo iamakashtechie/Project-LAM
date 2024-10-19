04c480731fad6602df0b92d46f22e49fd9ff10e8
iamakashtechie <akash.techie01@gmail.com>
2024-10-19 18:06:15 +0530

    fixed spotify api

---
+++ api/get-token.js
@@ -0,0 +1,19 @@
+// api/get-token.js
+const fetch = require('node-fetch');
+
+module.exports = async (req, res) => {
+    const clientId = process.env.SPOTIFY_CLIENT_ID;
+    const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+
+    const response = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64')
+        },
+        body: 'grant_type=client_credentials'
+    });
+
+    const data = await response.json();
+    res.status(200).json({ token: data.access_token });
+};
